4ac70ff26cee30023b8924ec294f685ddba3e062
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-26 14:23:51 -0300

    agrege Edad, Email, DNI y mensaje

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const usuarioNombre = document.querySelector("#usuario-nombre");
 const usuarioContrasena = document.querySelector("#usuario-contrasena");
+const usuarioEdad = document.querySelector("#usuario-edad");
+const usuarioEmail = document.querySelector("#usuario-email");
+const usuarioDni = document.querySelector("#usuario-dni");
+
 const pBienVenido = document.querySelector("#bien-venido");
 const cuerpoPagina = document.querySelector("body");
 const imagenCambio = document.querySelector("#img");
@@ -9,6 +13,9 @@ const CONTRASENA_USUARIO_1 = "123";
 
 let usuarioNombreIngresado = "def nombre";
 let usuarioContrasenaIngresada = "def contrasena";
+let usuarioEdadIngresada = "def edad";
+let usuarioEmailIngresado = "def email";
+let usuarioDniIngresado = "def DNI";
 
 const TIPO_FUENTE = "monospace";
 const IMG_1 = "https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg";
@@ -31,9 +38,18 @@ function login() {
 
     usuarioNombreIngresado = usuarioNombre.value;
     usuarioContrasenaIngresada = usuarioContrasena.value;
+    usuarioEdadIngresada = usuarioEdad.value;
+    usuarioEmailIngresado = usuarioEmail.value;
+    usuarioDniIngresado = usuarioDni.value;
 
     if ((usuarioNombreIngresado == USUARIO_1) && (usuarioContrasenaIngresada == CONTRASENA_USUARIO_1)) {
-        pBienVenido .innerHTML= "Bienvenido " + USUARIO_1;
+        let mensaje = 
+            "Bienvenido " + USUARIO_1 + "<br>" +
+            "Edad: " + usuarioEdadIngresada + "<br>" +
+            "Email: " + usuarioEmailIngresado + "<br>" +
+            "DNI: " + usuarioDniIngresado;
+        
+        pBienVenido.innerHTML= mensaje;
         modificarPagina(TIPO_FUENTE,COLOR_TXT_1,IMG_2);
     } else {
         pBienVenido.innerHTML = "Usuario o Contrasena incorrectos";
